769b56085a3b6ff60bb477531b0b748a9ba8cf9a
Tim Engler <engler@gmail.com>
2017-04-19 14:29:05 +0800

    About to get rid of the whole Either shortcut break stuff, since it's not needed as much as I thought

--- Main.hs
+++ Main.hs
@@ -19,17 +19,28 @@ data MyException = MyException String deriving (Show)
 instance Exception MyException 
 
 
+data MpvLoopAction = MpvLoopAction
+  { seekAction :: MpvLoop -> IO (), --action when we need to seek to the start of a loop 
+    playAction :: MpvLoop -> IO () --action to play the loop
+  }
+  
+type MpvLoop = Loop MpvLoopAction
 
-type MpvLoop = Loop (IO ())
-
+data MpvStatus =
+    MpvStart --begin
+  | MpvInLoop -- loop action run and inside of a loop
+  | MpvOutOfLoop -- defaultNoSrtAction run and outside of loop
+  | MpvEndLoop -- loop finished, and first of nextLoops moved to priorLoops, but
+               -- next action not run (which would be defaultNoSrtAction or the
+               -- next loops action, if outside or inside another loop respectively)
 data MpvState = MpvState {
-              ctx :: Ptr Ctx,
               priorLoops :: [MpvLoop], --loops already run
-              nextLoops :: [MpvLoop], --loops to run later
-              currLoop :: Maybe MpvLoop, --if processing a current loop that has started
-                                    --but not finished, this is it
+              nextLoops :: [MpvLoop], --loops to run later (or currently running)
+              status :: MpvStatus
               defaultNoSrtAction :: IO () -- if a loop finishes and there isn't another
                                        -- loop starting, we do this default action here
+              timeAction :: IO (Maybe Double) -- returns the current time. May return nothing
+                                              -- if unknown
            }
 
 
@@ -38,20 +49,29 @@ data MpvState = MpvState {
 --We use an EitherT to support this. left means to break out and doing something
 --other than the main loop action
 --returns true if should loop again
-type MpvET = StateT MpvState (EitherT (IO Bool) IO) Bool
+type MpvET = StateT MpvState (EitherT (Bool,MpvState) IO) Bool
 
-getActiveLoop st = undefined
+
+getActiveLoop :: MpvState -> Maybe (Bool,MpvLoop)
+getActiveLoop st =
+  case (currLoop st) of
+    Nothing -> getNextLoop
+    Just loop -> Just (True,loop)
+  where
+    getNextLoop = case (nextLoops st) of
+                    [] -> Nothing
+                    (l : ls) -> Just (False,l)
+
+
 
 event_loop :: MpvState -> IO ()
 --event_loop = undefined
 event_loop mpvState =
-  do
-    x <- runEitherT (runStateT doit mpvState)
+  do 
+    x <- runEitherT (runStateT doit mpvState)       
     case x of
-       (Left c) ->
-         do
-           res <- c
-           if res Then event_loop 
+       (Left (True,mpvState)) -> event_loop mpvState
+       (Left (False,_)) -> return ()
        --true we loop again
        (Right (True, mpvState)) -> event_loop mpvState
        --false we quit out
@@ -60,13 +80,17 @@ event_loop mpvState =
   where
     -- --runs action and quits if Bool is true
     breakif :: Bool -> MpvET -> MpvET
-    breakif True action = action >>= (\res -> lift (left (return res)))
+    breakif True action =
+      do
+        res <- action
+        state <- get
+        lift (left (res,state))
     breakif False _ = lift $ return True
 
     -- --runs action and quits if Bool is true
-    breakif2 :: Bool -> IO Bool -> MpvET
-    breakif2 True action = lift $ left action
-    breakif2 False _ = lift $ return True
+    -- breakif2 :: Bool -> IO Bool -> MpvET
+    -- breakif2 True action = lift $ left action
+    -- breakif2 False _ = lift $ return True
 
     -- --waits the given amount of time (or until the next event) and
     -- --recalls doit
@@ -75,43 +99,96 @@ event_loop mpvState =
       do
         st <- get
         event <- liftIO $ (mpv_wait_event (ctx st) $ realToFrac wait_time) >>= peek
-        breakif2 ((event_id event) == mpvEventShutdown)
-           (do
+        breakif ((event_id event) == mpvEventShutdown)
+           (lift $ lift (do
                      putStrLn "Bye!"
-                     return False)
+                     return False))
           
         --restart the event_loop, so it can recheck, since we may not have waited the
         --complete time because of some other event
-        lift $ left $ return True --quit out after we've run doit
+        lift $ left $ (True,st) --quit out after we've run doit
 
     --if wait time is positive, will wait and loop back to start
     --else no-op
     waitForTime :: Double -> Double -> MpvET
-    waitForTime = undefined
-    -- waitForTime st ct et =
-    --   do
-    --     timeToWait <- return et - ct
-    --     if (timeToWait > 0)
-    --       then
-    --         waitAndLoop timeToWait
-    --       else
-    --         right $ return () --no reason to wait, so continue
+    waitForTime ct et =
+      let timeToWait = (et-ct)
+          in
+            do
+              if (timeToWait > 0)
+              then
+                waitAndLoop timeToWait
+              else
+                lift $ return True --no reason to wait, so continue
 
- 
+    restartLoop :: MpvET
+    restartLoop =
+         do
+            st <- get
+            return (True, st)
+
+
     doit :: MpvET
     doit =
       do
         st <- get
-        ctx <- return (ctx st)
-        time <- liftIO $ mpv_get_property_double ctx "time-pos"
+--        time <- liftIO $ mpv_get_property_double ctx "time-pos"
+        time <- liftIO $ (timeAction st)
         breakif (time == Nothing) (waitAndLoop 1)
-        (Just jtime) <- return time
-        
-        (inLoop, loop) <- return (getActiveLoop st)
+        (Just jtime) <- return (realToFrac time)
 
-        waitForTime (realToFrac jtime) (if inLoop then (endTime loop) else (startTime loop))
-        return True
+        --find the current loop
+        nextLoop <- return (head (nextLoops st))
+
+        --perform the next action depending on the state
+        case (status st) of
+          MpvStart ->
+              do
+                liftIO $ (defaultNoSrtAction st)
+                put $ st {status=MpvOutOfLoop}
+          MpvOutOfLoop ->
+              do
+                waitForTime jtime (startTime loop)
+                liftIO $ (playAction (action loop))
+                put $ st {status=MpvInLoop}
+          MpvInLoop ->
+              do
+                waitForTime jtime (endTime loop)
+                put $ st {status=MpvEndLoop,
+                          priorLoops = loop : (priorLoops st),
+                          nextLoops = tail (nextLoops st),
+                         }
+          MpvEndLoop ->
+              do
+                if jtime < (startTime loop)
+                then
+                  do
+                    liftIO $ (defaultNoSrtAction st)
+                    put $ st {status=MpvOutOfLoop}
+                else
+                  do
+                    liftIO $ (seekAction (action loop)) >> (playAction (action loop))
+                    put $ st {status=MpvInLoop}
+
+                
+          
         
+        Just (inLoop, loop) <- return maybeLoop
+
+        --if we are currently processing an event
+        if inLoop
+        then
+          do
+             waitForTime (realToFrac jtime) (endTime loop) --wait for the loop to finish
+
+             --move to the next loop
+             newSt <- return st { nextLoops = tail (nextLoops st),
+                                  priorLoops = (head (nextLoops st)) : (priorLoops st),
+                                  status = MpvEndLoop}
+             put newSt
+             left $ return (True, newSt) --restart
+        else return ()
+
         
         
      
