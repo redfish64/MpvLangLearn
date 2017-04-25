0e912605e5d763eddc59a679a68460b32034f1ab
Tim Engler <engler@gmail.com>
2017-04-25 10:06:06 +0800

    did some more gui work but not finished. about to change how subtitles order, and lead and tail times are calculated

--- mll.py
+++ mll.py
@@ -18,24 +18,18 @@ def main():
   simple = sp.add_parser('simple', help='Simple options for running program')
 
   simple.add_argument("Video", help="Video to play", widget="FileChooser",default="a")
-  simple.add_argument("Srt1", help="First Subtitle file", widget="FileChooser",default="b")
-  simple.add_argument("Srt2", help="Second Subtitle file",default=srtNone, widget="FileChooser")
+  simple.add_argument("ForeignSrt", help="Foreign Language Subtitle (.srt) file (if any)", widget="FileChooser",,default=srtNone)
+  simple.add_argument("NativeSrt", help="Native Language Subtitle (.srt) file", widget="FileChooser")
   
   advanced = sp.add_parser('advanced', help='Advanced options for running program')
-
   advanced.add_argument("Video", help="Video to play", widget="FileChooser",default="a")
-  advanced.add_argument("SrtFile1", help="First Subtitle file", widget="FileChooser",default="b")
-  advanced.add_argument("SrtFile2", help="Second Subtitle file",default="<none>", widget="FileChooser")
-  advanced.add_argument('-t1sid','--FirstTrackSrtId', choices=['none','1','2','<disabled>'], default='none', help='Srt for first track. "none" means not display subtitles')
-  advanced.add_argument('-t1sp','--FirstTrackSpeed', choices=['1.5','1.25','1.0','0.9','0.75'], default='1.0', help='First Track Speed')
-  advanced.add_argument('-t2sid','--SecondTrackSrtId', choices=['none','1','2','<disabled>'], default='1', help='Srt for second track.')
-  advanced.add_argument('-t2sp','--SecondTrackSpeed', choices=['1.5','1.25','1.0','0.9','0.75'], default='1.0', help='Second Track Speed')
-
-  advanced.add_argument('-t3id', '--ThirdTrackSrtId', choices=['none','1','2','<disabled>'], default='2', help='Srt for third track. (optional)')
-  #group = advanced.add_argument_group('group') #does nothing
-  advanced.add_argument('-t3sp', '--ThirdTrackSpeed', choices=['1.5','1.25','1.0','0.9','0.75'], default='1.0', help='Third Track Speed (optional)')
   advanced.add_argument('-ls', '--LeadSeconds', type=float, default=1.0, help='Seconds before subtitle starts to start loop')
   advanced.add_argument('-ts', '--TailSeconds', type=float, default=1.0, help='Seconds after subtitle ends to end loop')
+  advanced.add_argument("EnableEmptyLoop", action="store_true", help="Enable a loop with no subtitles")
+  advanced.add_argument("Loop1Srt", help="First Subtitle file", widget="FileChooser",default="b")
+  advanced.add_argument('-t1sp','--FirstTrackSpeed', choices=['1.5','1.25','1.0','0.9','0.75'], default='1.0', help='First Track Speed')
+  advanced.add_argument("Loop2Srt", help="Second Subtitle file",default="<none>", widget="FileChooser")
+  advanced.add_argument('-t2sp','--SecondTrackSpeed', choices=['1.5','1.25','1.0','0.9','0.75'], default='1.0', help='Second Track Speed')
 
   args = p.parse_args()
   currdir = os.path.dirname(os.path.realpath(__file__))
