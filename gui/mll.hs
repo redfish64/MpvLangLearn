670807b8c3aa2f0b53702d04d9246714797d1c72
Tim Engler <engler@gmail.com>
2017-04-26 10:53:25 +0800

    moving to rascally haskelly wx

---
+++ gui/mll.hs
@@ -0,0 +1,117 @@
+module Main where
+
+import Data.Bits
+
+import Graphics.UI.WX
+import Graphics.UI.WXCore
+-- import Graphics.UI.WXCore.WxcDefs
+-- import Graphics.UI.WXCore.Frame
+-- import Graphics.UI.WXCore.WxcClassesAL
+-- import Graphics.UI.WXCore.WxcClassesMZ
+-- import Graphics.UI.WXCore.WxcTypes
+
+main :: IO ()
+main = start gui
+
+gui :: IO ()
+gui = do
+    -- hbox <- boxSizerCreate wxVERTICAL
+
+    f <- frame [text := "Mpv Lang Learn"]
+    p <- panel f []
+    nb <- notebook p []
+    px <- panel nb []
+    s <- simple px
+    --t <- staticText p1 [text := "Hello World"]
+    --    set p [layout := fill $ container p $ margin 10 $ widget t ]
+    --return p
+
+    set f [layout :=
+             container p $
+             column 0
+              [ tabs nb
+                [ tab "Simple" $ fill $ container px $ fill $ widget s ] ],
+
+            clientSize := sz 400 300 ]
+
+              -- t <- staticText p [text := "Hello World"]    
+     -- quitButton <- button window [text := "Quit", on command := close window]
+    -- exitButton <- button window [text := "Exit", on command := close window]
+    -- windowSetSizer window hbox
+    -- sizerAddWindow hbox exitButton 1 (wxEXPAND .|. wxALL) 5 ptrNull
+    -- sizerAddWindow hbox quitButton 1 (wxEXPAND .|. wxALL) 5 ptrNull
+    -- frameCenter window
+    return ()
+
+videoFiles
+   = [("Video files",["*.mkv","*.mp4","*.avi","*.wmv","*.flv","*.mov"])
+     ,("All files",["*.*"])
+     ]
+
+srtFiles
+   = [("Srt files",["*.srt"])
+     ,("All files",["*.*"])
+     ]
+
+
+simple :: Window a -> IO (Panel ())
+simple w =
+  do
+    p <- panel w []
+    videoField <- labeledField p "Video" "Video to play" (fileChooser "Open Video" videoFiles)
+    nativeLangSrtField <- labeledField p "Native Srt file" "Subtitle file in the native language of the user (srt format)" (fileChooser "Open Native Srt" srtFiles)
+    foreignLangSrtField <- labeledField p "Foreign Srt file" "Subtitle file in the language of the movie, optional (srt format)" (fileChooser "Open Foreign Srt" srtFiles)
+
+    okButton <- button p [ text := "O-Kay!" ]
+    cancelButton <- button p [ text := "Not Want" ]
+
+    set p [layout := margin 10 $ stretch $ fill $ column 4 $
+        (fmap (stretch . fill . widget . fieldPanel) [videoField, nativeLangSrtField, foreignLangSrtField]) ++
+          [space 1 30,alignBottomRight $ row 3 [widget cancelButton,  space 10 1, widget okButton]]
+        ]
+          
+    return p
+
+
+data Field a = Field { fieldPanel :: Panel (), fieldVal :: IO a }
+
+labeledField :: Window a -> String -> String -> (Panel () -> IO (Field x)) -> IO (Field x)
+labeledField w name desc ff =
+  do
+    p <- panel w []
+    n <- staticText p [text := name]
+    d <- staticText p [text := desc]
+    field <- ff p
+    set n [ fontWeight := WeightBold ]
+    set p [ layout := hfill $ column 3 [ fill $ widget n, fill $ widget d, fill $ widget (fieldPanel field) ] ]
+    return $ field { fieldPanel = p }
+
+
+
+fileChooser :: String -> [(String,[String])] -> Panel () -> IO (Field String )
+fileChooser title fileTypes w = 
+    do
+      p <- panel w []
+      i <- textEntry p []
+      b <- button p [ text := "Browse"]
+
+      set b [on command :=
+                 do
+                   maybeFile <- fileOpenDialog w False {- change current directory -}
+                                               True title fileTypes "" ""
+                   case maybeFile of
+                      Nothing -> return ()
+                      Just f -> set i [text := f] >> return ()
+            ]
+
+      set p [layout := hfill $ row 2 [ stretch $ fill $ widget i, space 10 1, widget b]]           
+                      
+
+      return $ Field p (get i text)
+    
+
+-- headerPanel = do
+--     p1   <- panel  nb []
+--        ok   <- button p1 [text := "Ok", on command := logMessage "ok button pressed"]
+--        quit <- button p1 [text := "Quit", on command := close f]
+
--- mll.py
+++ mll.py
@@ -7,7 +7,7 @@ import sys
 nonbuffered_stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 sys.stdout = nonbuffered_stdout
   
-@Gooey(program_name="MpvLangLearn",required_cols=1,optional_cols=2,advanced=True)
+@Gooey(program_name="MpvLangLearn",required_cols=1,optional_cols=1,advanced=True)
 def main():
   desc = "\nLearn a foreign language by playing movies"
   srtNone = "<none>"
@@ -17,48 +17,49 @@ def main():
 
   simple = sp.add_parser('simple', help='Simple options for running program')
 
-  simple.add_argument("Video", help="Video to play", widget="FileChooser",default="a")
-  simple.add_argument("ForeignSrt", help="Foreign Language Subtitle (.srt) file (if any)", widget="FileChooser",,default=srtNone)
+  simple.add_argument("Video", help="Video to play", widget="FileChooser")
   simple.add_argument("NativeSrt", help="Native Language Subtitle (.srt) file", widget="FileChooser")
+  simple.add_argument("-fs","--ForeignSrt", help="Foreign Language Subtitle (.srt) file. (Will be displayed before Native srt file)", widget="FileChooser")
   
   advanced = sp.add_parser('advanced', help='Advanced options for running program')
   advanced.add_argument("Video", help="Video to play", widget="FileChooser",default="a")
-  advanced.add_argument('-ls', '--LeadSeconds', type=float, default=1.0, help='Seconds before subtitle starts to start loop')
-  advanced.add_argument('-ts', '--TailSeconds', type=float, default=1.0, help='Seconds after subtitle ends to end loop')
-  advanced.add_argument("EnableEmptyLoop", action="store_true", help="Enable a loop with no subtitles")
-  advanced.add_argument("Loop1Srt", help="First Subtitle file", widget="FileChooser",default="b")
-  advanced.add_argument('-t1sp','--FirstTrackSpeed', choices=['1.5','1.25','1.0','0.9','0.75'], default='1.0', help='First Track Speed')
-  advanced.add_argument("Loop2Srt", help="Second Subtitle file",default="<none>", widget="FileChooser")
-  advanced.add_argument('-t2sp','--SecondTrackSpeed', choices=['1.5','1.25','1.0','0.9','0.75'], default='1.0', help='Second Track Speed')
+  advanced.add_argument("EnableEmptyLoop", action="store_true", help="Enable a loop with no subtitles. Otherwise subtitles will always be displayed (either first or second)")
+  advanced.add_argument('EmptyTrackSpeed', choices=['1.5','1.25','1.0','0.9','0.75'], default='1.0', help='If Empty Loop enabled, speed of video while no subtitle is displayed (only while people are talking)')
+  advanced.add_argument("Loop1Srt", help="These subtitles will be displayed first", widget="FileChooser",default="b")
+  advanced.add_argument('FirstSrtSpeed', choices=['1.5','1.25','1.0','0.9','0.75'], default='1.0', help='Speed of video while showing subtitles from first subtitle file')
+  advanced.add_argument('-l2s',"--Loop2Srt", help="Second Subtitle file",widget="FileChooser")
+  advanced.add_argument('-sss','--SecondSrtSpeed', choices=['1.5','1.25','1.0','0.9','0.75'], default='1.0', help='Speed of video while showing subtitles from second subtitle file')
+  advanced.add_argument('-ls','--LeadSeconds', type=float, default=1.0, help='Seconds before each subtitle starts to start loop')
+  advanced.add_argument('-ts','--TailSeconds', type=float, default=1.0, help='Seconds after each subtitle ends to wait to end loop')
 
   args = p.parse_args()
   currdir = os.path.dirname(os.path.realpath(__file__))
 
-  if args.command == "simple":
-    args.FirstTrackSpeed='1.0'
-    args.FirstTrackSrtId='none'
-    args.LeadSeconds=1.0
-    args.SecondTrackSpeed='1.0'
-    args.SecondTrackSrtId='1'
-    args.TailSeconds=1.0
-    if(args.SrtFile2 != srtNone && isfile args.SrtFile2):
-      args.ThirdTrackSpeed='1.0'
-      args.ThirdTrackSrtId='2'
-    else:
-      args.ThirdTrackSrtId='<disabled>'
-  else:
-    args.FirstTrackSpeed='1.0'
-    args.FirstTrackSrtId='none'
-    args.LeadSeconds=1.0
-    args.SecondTrackSpeed='1.0'
-    args.SecondTrackSrtId='1'
-    args.TailSeconds=1.0
-    if(args.SrtFile2 == srtNone || !(isfile args.SrtFile2)):
-      args.ThirdTrackSrtId='<disabled>'
+  # if args.command == "simple":
+  #   args.FirstTrackSpeed='1.0'
+  #   args.FirstTrackSrtId='none'
+  #   args.LeadSeconds=1.0
+  #   args.SecondTrackSpeed='1.0'
+  #   args.SecondTrackSrtId='1'
+  #   args.TailSeconds=1.0
+  #   if(args.SrtFile2 != srtNone && isfile args.SrtFile2):
+  #     args.ThirdTrackSpeed='1.0'
+  #     args.ThirdTrackSrtId='2'
+  #   else:
+  #     args.ThirdTrackSrtId='<disabled>'
+  # else:
+  #   args.FirstTrackSpeed='1.0'
+  #   args.FirstTrackSrtId='none'
+  #   args.LeadSeconds=1.0
+  #   args.SecondTrackSpeed='1.0'
+  #   args.SecondTrackSrtId='1'
+  #   args.TailSeconds=1.0
+  #   if(args.SrtFile2 == srtNone || !(isfile args.SrtFile2)):
+  #     args.ThirdTrackSrtId='<disabled>'
 
       
   print args
-  os.execv(currdir+'/fakeout',args)
+  # os.execv(currdir+'/fakeout',args)
 
 def here_is_smore():
   pass
