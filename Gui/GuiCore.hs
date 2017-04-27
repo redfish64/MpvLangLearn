6a961e960156df5bc52092fad25dec9ab50ac6bb
Tim Engler <engler@gmail.com>
2017-04-27 09:35:20 +0800

    Simple gui seems to work

--- Gui/GuiCore.hs
+++ Gui/GuiCore.hs
@@ -1,17 +1,21 @@
-module Main where
+module Gui.GuiCore(startGui) where
 
 import Data.Bits
+import Text.Printf (printf)
+import Core(commandLine)
 
 import Graphics.UI.WX
 import Graphics.UI.WXCore
+import Control.Concurrent(forkIO,forkOS)
 -- import Graphics.UI.WXCore.WxcDefs
 -- import Graphics.UI.WXCore.Frame
 -- import Graphics.UI.WXCore.WxcClassesAL
 -- import Graphics.UI.WXCore.WxcClassesMZ
 -- import Graphics.UI.WXCore.WxcTypes
 
-main :: IO ()
-main = start gui
+startGui :: IO ()
+startGui = start gui
+
 
 gui :: IO ()
 gui = do
@@ -21,7 +25,8 @@ gui = do
     p <- panel f []
     nb <- notebook p []
     px <- panel nb []
-    s <- simple px
+    --    s <- fakeout px
+    s <- simple f px
     --t <- staticText p1 [text := "Hello World"]
     --    set p [layout := fill $ container p $ margin 10 $ widget t ]
     --return p
@@ -30,9 +35,9 @@ gui = do
              container p $
              column 0
               [ tabs nb
-                [ tab "Simple" $ stretch $ container px $ widget s ] ],
+                [ tab "Simple" $ container px $ fill $ widget s ] ],
 
-            clientSize := sz 400 300 ]
+            clientSize := sz 400 350 ]
 
               -- t <- staticText p [text := "Hello World"]    
      -- quitButton <- button window [text := "Quit", on command := close window]
@@ -54,26 +59,58 @@ srtFiles
      ]
 
 
-simple :: Window a -> IO (Panel ())
-simple w =
+simple :: Frame x -> Window a -> IO (Panel ())
+simple f w =
   do
     p <- panel w []
+    -- f <- fakeout p
+    -- set p [ layout := margin 10 $ widget f]
+
     videoField <- labeledField p "Video" "Video to play" (fileChooser "Open Video" videoFiles)
+    -- set p [ layout := margin 10 $ column 4 [ fill $ widget $ fieldPanel videoField] ]
+    --(widget (fieldPanel videoField))
+
+    
     nativeLangSrtField <- labeledField p "Native Srt file" "Subtitle file in the native language of the user (srt format)" (fileChooser "Open Native Srt" srtFiles)
     foreignLangSrtField <- labeledField p "Foreign Srt file" "Subtitle file in the language of the movie, optional (srt format)" (fileChooser "Open Foreign Srt" srtFiles)
 
-    okButton <- button p [ text := "O-Kay!" ]
-    cancelButton <- button p [ text := "Not Want" ]
+    okButton <- button p [ text := "OK"]
 
+    set okButton  [on command := do
+                      set f [ on idle :=
+                              do
+                                runSimple videoField nativeLangSrtField foreignLangSrtField
+                                set f [ on idle := return False ]
+                                return False
+                            ]
+                  ]
     set p [layout := margin 10 $ column 4 $
-        (fmap (widget . fieldPanel) [videoField, nativeLangSrtField, foreignLangSrtField]) ++
-          [space 1 30,alignBottomRight $ row 3 [widget cancelButton,  space 10 1, widget okButton]]
+        (fmap (hfill. widget . fieldPanel) [videoField, nativeLangSrtField, foreignLangSrtField]) ++
+          [fill $ space 1 30,hfill $ alignBottomRight $ widget okButton]
         ]
           
     return p
 
+runSimple :: Field String -> Field String -> Field String -> IO ()
+runSimple vf nlf flf =
+    do
+      video <- (fieldVal vf)
+      nlSrt <-  (fieldVal nlf)
+      flSrt <-  (fieldVal flf)
+      putStrLn (printf "runSimple: %s %s %s" video nlSrt flSrt)
+      --forkIO $
+      commandLine [flSrt,nlSrt,"--","none:1:1:1","1:1:1:1","2:1:1:1","--",video]
+      return ()
+      
+
 
 data Field a = Field { fieldPanel :: Panel (), fieldVal :: IO a }
+
+fakeout :: Panel () -> IO (Panel ())
+fakeout w = do
+               p <- panel w []
+               set p [layout := fill $ boxed "fakeout!" $ (space 1 1)]
+               return p
 
 labeledField :: Window a -> String -> String -> (Panel () -> IO (Field x)) -> IO (Field x)
 labeledField w name desc ff =
@@ -83,7 +120,9 @@ labeledField w name desc ff =
     d <- staticText p [text := desc]
     field <- ff p
     set n [ fontWeight := WeightBold ]
-    set p [ layout := column 3 [ widget n, widget d, widget (fieldPanel field) ] ]
+    set p [ layout := column 3 [ hfill $ widget n, hfill $ widget d, hfill $ widget
+       (fieldPanel field)
+        ] ]
     return $ field { fieldPanel = p }
 
 
@@ -104,7 +143,7 @@ fileChooser title fileTypes w =
                       Just f -> set i [text := f] >> return ()
             ]
 
-      set p [layout := hfill $ row 2 [ widget i, space 10 1, widget b]]           
+      set p [layout := hfill $ row 2 [ hfill $ widget i, space 10 1, widget b]]           
                       
 
       return $ Field p (get i text)
---
+++ GuiMain.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Gui.GuiCore
+
+main :: IO ()
+main = startGui
